2de8b3611131c6f38a8070dfa4fd68bb0f923641
mingyoung <alsrud0205@naver.com>
2024-05-15 15:06:54 +0900

    VerifyNum, ResetPw

--- src/screens/login/ResetPassword.js
+++ src/screens/login/ResetPassword.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
-const ResetPassword = () => {
+const ResetPassword = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Reset Password Screen</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.resetPw}>비밀번호 재설정 완료</Text>
+        <Text style={styles.description}>
+          임시 비밀번호가 발급되었습니다.{'\n'}
+        </Text>
+        <Text style={styles.description}>메일을 확인해주세요!</Text>
+      </View>
+      <ImageBackground
+        source={require('../../assets/passwordRe.png')}
+        style={styles.image}
+        resizeMode="contain"
+      ></ImageBackground>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.back}
+          onPress={() => navigation.navigate('Login')}
+        >
+          <Text style={styles.buttonText}>되돌아가기</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -12,8 +39,50 @@ const ResetPassword = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+
+  image: {
+    width: '100%',
+    height: '50%',
+    marginTop: 10,
+  },
+
+  textContainer: {
+    marginBottom: 110,
+    marginTop: '30%',
+    marginLeft: '8%',
+  },
+
+  resetPw: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+
+  description: {
+    fontSize: 15,
+    marginBottom: -12,
+  },
+
+  buttonContainer: {
+    alignItems: 'center',
+  },
+
+  back: {
+    marginTop: -40,
+    height: 47,
+    width: '83%',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#75C743',
+    borderRadius: 16,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 })
 
--- src/screens/login/VerifyNumber.js
+++ src/screens/login/VerifyNumber.js
@@ -6,20 +6,43 @@ import {
   View,
   TouchableWithoutFeedback,
   Keyboard,
+  Modal,
 } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { TextInput } from 'react-native-gesture-handler'
 import Header from '../../components/Header'
 import ResetPassword from '../login/ResetPassword'
+import { func } from 'prop-types'
 
 const VerifyNumber = ({ navigation, route }) => {
   const { userMail } = route.params
   const [isFocused, setIsFocused] = useState(false)
   const [verifiCode, setVerifiCode] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+  //   const [randomCode, setRandomCode] = useState(generateRandomCode())
+
+  // 랜덤 코드 6자리
+  //   const generateRandomCode = () => {
+  //     return Math.floor(100000 + Math.random() * 900000).toString()
+  //   }
+
+  const randomCode = '121212'
 
   // 인증번호 입력
   const handleVeifiCodeChange = (text) => {
     setVerifiCode(text)
+  }
+
+  // 인증번호 확인
+  const checkVerifiCode = () => {
+    // const randomCode = generateRandomCode()
+
+    // if (verifiCode === randomCode) {
+    if (verifiCode === randomCode) {
+      navigation.navigate('ResetPassword')
+    } else {
+      setModalVisible(true)
+    }
   }
 
   // 계속하기
@@ -70,12 +93,45 @@ const VerifyNumber = ({ navigation, route }) => {
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={getButtonStyle()}
-            onPress={() => navigation.navigate('ResetPassword')}
+            onPress={checkVerifiCode}
             disabled={verifiCode.length !== 6}
           >
             <Text style={styles.buttonText}>계속하기</Text>
           </TouchableOpacity>
         </View>
+
+        {/* 인증 실패 */}
+        <Modal
+          // animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(true)
+          }}
+        >
+          <View style={styles.modalView}>
+            <View style={styles.modal}>
+              <Text style={styles.modalText}>
+                올바르지 않은 인증번호입니다.
+              </Text>
+              <View style={styles.modalButtonContainer}>
+                <TouchableOpacity
+                  style={styles.newMail}
+                  onPress={() => setModalVisible(false)}
+                >
+                  <Text style={styles.newMailText}>새 메일 보내기</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.cancel}
+                  onPress={() => setModalVisible(false)}
+                >
+                  <Text style={styles.cancelText}>취소</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
     </TouchableWithoutFeedback>
   )
@@ -154,18 +210,71 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 
-  noAccount: {
-    flexDirection: 'row',
+  modalView: {
+    flex: 1,
     justifyContent: 'center',
-    marginTop: 10,
-    fontSize: 16,
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'rgba(0, 0, 0, 0.5',
   },
 
-  join: {
-    textDecorationLine: 'underline',
+  modal: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    width: '70%',
+    height: '16%',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 16,
     fontWeight: 'bold',
-    marginLeft: 15,
+  },
+
+  modalButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  newMail: {
+    alignItems: 'center',
+    padding: 10,
+    marginRight: 10,
+    backgroundColor: '#75C743',
+    borderRadius: 8,
+    width: 110,
+  },
+
+  cancel: {
+    alignItems: 'center',
+    padding: 10,
+    borderColor: '#75C743',
+    borderWidth: 1,
+    borderRadius: 8,
+    width: 110,
+  },
+
+  newMailText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+
+  cancelText: {
+    color: '#75C743',
+    fontWeight: 'bold',
+    fontSize: '14',
   },
 })
-
 export default VerifyNumber
