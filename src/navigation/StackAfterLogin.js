8d15f531703139fa3f804af6ec7ed578d58e66fb
mingyoung <alsrud0205@naver.com>
2024-07-01 11:35:46 +0900

    RecruitDetails host, participants

--- src/navigation/StackAfterLogin.js
+++ src/navigation/StackAfterLogin.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import TabNavigation from './TabNavigation';
-import MyInfoCheck from '../screens/mypage/MyInfoCheck';
-import SignOut from '../screens/mypage/SignOut';
-import SignOutFinish from '../screens/mypage/SignOutFinish';
-import Search from '../screens/main/Search';
-import SearchResult from '../screens/main/SearchResult';
-import RecruitDetails from '../screens/main/recruit/RecruitDetalis';
-import Useredit from '../screens/mypage/Useredit';
-import Phoneedit from '../screens/mypage/Phoneedit';
-import Passwordedit from '../screens/mypage/Passwordedit';
-import Editend from '../screens/mypage/Editend';
-import NotificationCheck from '../screens/mypage/NotificationCheck';
-import MyInfo from '../screens/mypage/MyInfo';
-import FAQ from '../screens/mypage/FAQ';
-import CreateGroupPurchase from '../screens/main/participate/CreateGroupPurchase';
-import ParticipantInfo from '../screens/main/participate/ParticipantInfo';
-import ManageMy from '../screens/main/ManageMy';
-import FinishedMy from '../screens/main/FinishedMy';
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import TabNavigation from './TabNavigation'
+import MyInfoCheck from '../screens/mypage/MyInfoCheck'
+import SignOut from '../screens/mypage/SignOut'
+import SignOutFinish from '../screens/mypage/SignOutFinish'
+import Search from '../screens/main/Search'
+import SearchResult from '../screens/main/SearchResult'
+import RecruitDetails from '../screens/main/recruit/RecruitDetalis'
+import Useredit from '../screens/mypage/Useredit'
+import Phoneedit from '../screens/mypage/Phoneedit'
+import Passwordedit from '../screens/mypage/Passwordedit'
+import Editend from '../screens/mypage/Editend'
+import NotificationCheck from '../screens/mypage/NotificationCheck'
+import MyInfo from '../screens/mypage/MyInfo'
+import FAQ from '../screens/mypage/FAQ'
+import CreateGroupPurchase from '../screens/main/participate/CreateGroupPurchase'
+import ParticipantInfo from '../screens/main/participate/ParticipantInfo'
+import ManageMy from '../screens/main/ManageMy'
+import FinishedMy from '../screens/main/FinishedMy'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 const StackAfterLogin = () => {
   return (
-    <Stack.Navigator initialRouteName="TabNavigation">
+    <Stack.Navigator initialRouteName="RecruitDetails">
       <Stack.Screen
         name="TabNavigation"
         component={TabNavigation}
@@ -121,7 +121,7 @@ const StackAfterLogin = () => {
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
-  );
-};
+  )
+}
 
-export default StackAfterLogin;
+export default StackAfterLogin
